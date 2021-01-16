e721b39b6b880013ff3a33a8bced8a29b9d646df
mihowilk <mihowilk@gmail.com>
2021-01-16 21:48:37 +0100

    refactor files

--- libraries/utils.py
+++ libraries/utils.py
@@ -3,5 +3,5 @@ import json
 
 def save_to_json(filename, content):
     json_data = json.dumps(content)
-    with open(filename, 'w') as f:
+    with open("../"+filename, 'w') as f:
         f.write(content)
