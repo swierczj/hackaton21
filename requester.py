5ddd3517d0407ad959581468108b0a9f71bc60e0
mihowilk <mihowilk@gmail.com>
2021-01-16 21:32:40 +0100

    small refactor

--- requester.py
+++ requester.py
@@ -1,12 +1,7 @@
 from googlesearch import search
-import json
 from data_models import Data, DataFrame, Answer
 from scrapper import scrap_data
-
-
-def save_to_json(filename, content):
-    with open(filename, 'w') as f:
-        f.write(content)
+from utils import save_to_json
 
 
 def get_query():
@@ -24,10 +19,7 @@ def send_requests(site):
             dataframe.add_answer(scrap_data(URL).__dict__)
         data.add_dataframe(dataframe.__dict__)
 
-    json_data = json.dumps(data.__dict__)
-
-    # print("const data = "+json_data)
-    save_to_json("data.json", json_data)
+    save_to_json("data.json", data.__dict__)
 
 
 if __name__ == "__main__":
--- scrapper.py
+++ scrapper.py
@@ -3,10 +3,6 @@ from bs4 import BeautifulSoup
 from data_models import Answer
 
 
-def get_url():
-    return 'https://stackoverflow.com/questions/9662346/python-code-to-remove-html-tags-from-a-string'
-
-
 def scrap_data(URL):
     page = requests.get(URL)
 
@@ -20,11 +16,9 @@ def scrap_data(URL):
         result = results.find('div', 'js-post-body')
         votes = results.find('div', class_="ai-center")
         user_name = results.find('div', class_="user-details").find('a')
-        with open("scrapped_data.txt", "w") as f:
-            f.write(result.get_text()[:500])
         return Answer(URL, result, votes, user_name)
 
 
 if __name__ == "__main__":
-    url = get_url()
+    url = 'https://stackoverflow.com/questions/9662346/python-code-to-remove-html-tags-from-a-string'
     scrap_data(url)
